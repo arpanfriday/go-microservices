d2a679a09443c2682090eab4f1b44a5d31452614
Arpan Maity <arpanriju2000@gmail.com>
2025-10-08 21:34:05 +0530

    Update allowed methods in CORS and enhance test page functionality for broker service

--- broker-service/cmd/api/routes.go
+++ broker-service/cmd/api/routes.go
@@ -14,7 +14,7 @@ func (app *Config) routes() http.Handler {
 	// Specify who is allowed to connect
 	mux.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"https://*", "http://*"},
-		AllowedMethods:   []string{"GET", "PUT", "DELETE", "OPTIONS"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: true,
