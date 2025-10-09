e9073ae177f02c14e6997a64f927b6e5f151bc1b
Arpan Maity <arpanriju2000@gmail.com>
2025-10-09 16:20:24 +0530

    code modification to help out app connect to the postgreSQL database

--- authentication-service/cmd/api/main.go
+++ authentication-service/cmd/api/main.go
@@ -6,9 +6,17 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5"
 )
 
 const webPort = "80"
+const waitTillRetry = 2
+const maxRetries = 10
+
+var retries int64
 
 type Config struct {
 	DB     *sql.DB
@@ -19,9 +27,16 @@ func main() {
 	log.Println("Starting autentication service")
 
 	// TODO Connect to DB
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Cannot connect to Postgres")
+	}
 
 	// Setup config
-	app := Config{}
+	app := Config{
+		DB:     conn,
+		Models: data.New(conn),
+	}
 
 	srv := http.Server{
 		Addr:    fmt.Sprintf(":%s", webPort),
@@ -31,5 +46,41 @@ func main() {
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Panic(err)
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("PostgreSQL not yet ready")
+			retries++
+		} else {
+			log.Println("Connected to PostgreSQL")
+			return connection
+		}
+
+		if retries > maxRetries {
+			log.Println(err)
+			return nil
+		}
+
+		log.Printf("Retrying in %d seconds ... ", waitTillRetry)
+		time.Sleep(waitTillRetry * time.Second)
+		continue
 	}
 }
--- authentication-service/go.mod
+++ authentication-service/go.mod
@@ -7,4 +7,8 @@ require golang.org/x/crypto v0.43.0
 require (
 	github.com/go-chi/chi/v5 v5.2.3 // indirect
 	github.com/go-chi/cors v1.2.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.6 // indirect
+	golang.org/x/text v0.30.0 // indirect
 )
